f542a1f4c0fb437cf6f6755d4411cb4bf309c81b
rainwildest <rainwildest@163.com>
2023-02-01 08:54:36 +0800

    add tailwindcss classname

--- plug-in/data/index.ts
+++ plug-in/data/index.ts
@@ -1,73 +1,44 @@
 const data = [
-  "box-decoration-clone	box-decoration-break: clone;",
-  "box-decoration-slice	box-decoration-break: slice;"
+  "max-h-0	max-height: 0px;",
+  "max-h-px	max-height: 1px;",
+  "max-h-0.5	max-height: 0.125rem; /* 2px */",
+  "max-h-1	max-height: 0.25rem; /* 4px */",
+  "max-h-1.5	max-height: 0.375rem; /* 6px */",
+  "max-h-2	max-height: 0.5rem; /* 8px */",
+  "max-h-2.5	max-height: 0.625rem; /* 10px */",
+  "max-h-3	max-height: 0.75rem; /* 12px */",
+  "max-h-3.5	max-height: 0.875rem; /* 14px */",
+  "max-h-4	max-height: 1rem; /* 16px */",
+  "max-h-5	max-height: 1.25rem; /* 20px */",
+  "max-h-6	max-height: 1.5rem; /* 24px */",
+  "max-h-7	max-height: 1.75rem; /* 28px */",
+  "max-h-8	max-height: 2rem; /* 32px */",
+  "max-h-9	max-height: 2.25rem; /* 36px */",
+  "max-h-10	max-height: 2.5rem; /* 40px */",
+  "max-h-11	max-height: 2.75rem; /* 44px */",
+  "max-h-12	max-height: 3rem; /* 48px */",
+  "max-h-14	max-height: 3.5rem; /* 56px */",
+  "max-h-16	max-height: 4rem; /* 64px */",
+  "max-h-20	max-height: 5rem; /* 80px */",
+  "max-h-24	max-height: 6rem; /* 96px */",
+  "max-h-28	max-height: 7rem; /* 112px */",
+  "max-h-32	max-height: 8rem; /* 128px */",
+  "max-h-36	max-height: 9rem; /* 144px */",
+  "max-h-40	max-height: 10rem; /* 160px */",
+  "max-h-44	max-height: 11rem; /* 176px */",
+  "max-h-48	max-height: 12rem; /* 192px */",
+  "max-h-52	max-height: 13rem; /* 208px */",
+  "max-h-56	max-height: 14rem; /* 224px */",
+  "max-h-60	max-height: 15rem; /* 240px */",
+  "max-h-64	max-height: 16rem; /* 256px */",
+  "max-h-72	max-height: 18rem; /* 288px */",
+  "max-h-80	max-height: 20rem; /* 320px */",
+  "max-h-96	max-height: 24rem; /* 384px */",
+  "max-h-full	max-height: 100%;",
+  "max-h-screen	max-height: 100vh;",
+  "max-h-min	max-height: min-content;",
+  "max-h-max	max-height: max-content;",
+  "max-h-fit	max-height: fit-content;"
 ];
-// w-0	width: 0px;
-// w-px	width: 1px;
-// w-0.5	width: 0.125rem; /* 2px */
-// w-1	width: 0.25rem; /* 4px */
-// w-1.5	width: 0.375rem; /* 6px */
-// w-2	width: 0.5rem; /* 8px */
-// w-2.5	width: 0.625rem; /* 10px */
-// w-3	width: 0.75rem; /* 12px */
-// w-3.5	width: 0.875rem; /* 14px */
-// w-4	width: 1rem; /* 16px */
-// w-5	width: 1.25rem; /* 20px */
-// w-6	width: 1.5rem; /* 24px */
-// w-7	width: 1.75rem; /* 28px */
-// w-8	width: 2rem; /* 32px */
-// w-9	width: 2.25rem; /* 36px */
-// w-10	width: 2.5rem; /* 40px */
-// w-11	width: 2.75rem; /* 44px */
-// w-12	width: 3rem; /* 48px */
-// w-14	width: 3.5rem; /* 56px */
-// w-16	width: 4rem; /* 64px */
-// w-20	width: 5rem; /* 80px */
-// w-24	width: 6rem; /* 96px */
-// w-28	width: 7rem; /* 112px */
-// w-32	width: 8rem; /* 128px */
-// w-36	width: 9rem; /* 144px */
-// w-40	width: 10rem; /* 160px */
-// w-44	width: 11rem; /* 176px */
-// w-48	width: 12rem; /* 192px */
-// w-52	width: 13rem; /* 208px */
-// w-56	width: 14rem; /* 224px */
-// w-60	width: 15rem; /* 240px */
-// w-64	width: 16rem; /* 256px */
-// w-72	width: 18rem; /* 288px */
-// w-80	width: 20rem; /* 320px */
-// w-96	width: 24rem; /* 384px */
-// w-auto	width: auto;
-// w-1/2	width: 50%;
-// w-1/3	width: 33.333333%;
-// w-2/3	width: 66.666667%;
-// w-1/4	width: 25%;
-// w-2/4	width: 50%;
-// w-3/4	width: 75%;
-// w-1/5	width: 20%;
-// w-2/5	width: 40%;
-// w-3/5	width: 60%;
-// w-4/5	width: 80%;
-// w-1/6	width: 16.666667%;
-// w-2/6	width: 33.333333%;
-// w-3/6	width: 50%;
-// w-4/6	width: 66.666667%;
-// w-5/6	width: 83.333333%;
-// w-1/12	width: 8.333333%;
-// w-2/12	width: 16.666667%;
-// w-3/12	width: 25%;
-// w-4/12	width: 33.333333%;
-// w-5/12	width: 41.666667%;
-// w-6/12	width: 50%;
-// w-7/12	width: 58.333333%;
-// w-8/12	width: 66.666667%;
-// w-9/12	width: 75%;
-// w-10/12	width: 83.333333%;
-// w-11/12	width: 91.666667%;
-// w-full	width: 100%;
-// w-screen	width: 100vw;
-// w-min	width: min-content;
-// w-max	width: max-content;
-// w-fit	width: fit-content;
 
 export default data;
---
+++ views/low-code/components/AttributeArea/components/tailwindcss-classname/sizing.ts
@@ -0,0 +1,853 @@
+export const Width: AttributeProps = {
+  type: "width",
+  header: "Width 属性",
+  title: "Width ClassName：",
+  inputPlaceholder: "width: xxxx",
+  selectPlaceholder: "width: xxxx",
+  hasCustom: true,
+  options: [
+    {
+      value: "w-0",
+      label: "『 w-0 』 width: 0px;"
+    },
+    {
+      value: "w-px",
+      label: "『 w-px 』 width: 1px;"
+    },
+    {
+      value: "w-0.5",
+      label: "『 w-0.5 』 width: 0.125rem; /* 2px */"
+    },
+    {
+      value: "w-1",
+      label: "『 w-1 』 width: 0.25rem; /* 4px */"
+    },
+    {
+      value: "w-1.5",
+      label: "『 w-1.5 』 width: 0.375rem; /* 6px */"
+    },
+    {
+      value: "w-2",
+      label: "『 w-2 』 width: 0.5rem; /* 8px */"
+    },
+    {
+      value: "w-2.5",
+      label: "『 w-2.5 』 width: 0.625rem; /* 10px */"
+    },
+    {
+      value: "w-3",
+      label: "『 w-3 』 width: 0.75rem; /* 12px */"
+    },
+    {
+      value: "w-3.5",
+      label: "『 w-3.5 』 width: 0.875rem; /* 14px */"
+    },
+    {
+      value: "w-4",
+      label: "『 w-4 』 width: 1rem; /* 16px */"
+    },
+    {
+      value: "w-5",
+      label: "『 w-5 』 width: 1.25rem; /* 20px */"
+    },
+    {
+      value: "w-6",
+      label: "『 w-6 』 width: 1.5rem; /* 24px */"
+    },
+    {
+      value: "w-7",
+      label: "『 w-7 』 width: 1.75rem; /* 28px */"
+    },
+    {
+      value: "w-8",
+      label: "『 w-8 』 width: 2rem; /* 32px */"
+    },
+    {
+      value: "w-9",
+      label: "『 w-9 』 width: 2.25rem; /* 36px */"
+    },
+    {
+      value: "w-10",
+      label: "『 w-10 』 width: 2.5rem; /* 40px */"
+    },
+    {
+      value: "w-11",
+      label: "『 w-11 』 width: 2.75rem; /* 44px */"
+    },
+    {
+      value: "w-12",
+      label: "『 w-12 』 width: 3rem; /* 48px */"
+    },
+    {
+      value: "w-14",
+      label: "『 w-14 』 width: 3.5rem; /* 56px */"
+    },
+    {
+      value: "w-16",
+      label: "『 w-16 』 width: 4rem; /* 64px */"
+    },
+    {
+      value: "w-20",
+      label: "『 w-20 』 width: 5rem; /* 80px */"
+    },
+    {
+      value: "w-24",
+      label: "『 w-24 』 width: 6rem; /* 96px */"
+    },
+    {
+      value: "w-28",
+      label: "『 w-28 』 width: 7rem; /* 112px */"
+    },
+    {
+      value: "w-32",
+      label: "『 w-32 』 width: 8rem; /* 128px */"
+    },
+    {
+      value: "w-36",
+      label: "『 w-36 』 width: 9rem; /* 144px */"
+    },
+    {
+      value: "w-40",
+      label: "『 w-40 』 width: 10rem; /* 160px */"
+    },
+    {
+      value: "w-44",
+      label: "『 w-44 』 width: 11rem; /* 176px */"
+    },
+    {
+      value: "w-48",
+      label: "『 w-48 』 width: 12rem; /* 192px */"
+    },
+    {
+      value: "w-52",
+      label: "『 w-52 』 width: 13rem; /* 208px */"
+    },
+    {
+      value: "w-56",
+      label: "『 w-56 』 width: 14rem; /* 224px */"
+    },
+    {
+      value: "w-60",
+      label: "『 w-60 』 width: 15rem; /* 240px */"
+    },
+    {
+      value: "w-64",
+      label: "『 w-64 』 width: 16rem; /* 256px */"
+    },
+    {
+      value: "w-72",
+      label: "『 w-72 』 width: 18rem; /* 288px */"
+    },
+    {
+      value: "w-80",
+      label: "『 w-80 』 width: 20rem; /* 320px */"
+    },
+    {
+      value: "w-96",
+      label: "『 w-96 』 width: 24rem; /* 384px */"
+    },
+    {
+      value: "w-auto",
+      label: "『 w-auto 』 width: auto;"
+    },
+    {
+      value: "w-1/2",
+      label: "『 w-1/2 』 width: 50%;"
+    },
+    {
+      value: "w-1/3",
+      label: "『 w-1/3 』 width: 33.333333%;"
+    },
+    {
+      value: "w-2/3",
+      label: "『 w-2/3 』 width: 66.666667%;"
+    },
+    {
+      value: "w-1/4",
+      label: "『 w-1/4 』 width: 25%;"
+    },
+    {
+      value: "w-2/4",
+      label: "『 w-2/4 』 width: 50%;"
+    },
+    {
+      value: "w-3/4",
+      label: "『 w-3/4 』 width: 75%;"
+    },
+    {
+      value: "w-1/5",
+      label: "『 w-1/5 』 width: 20%;"
+    },
+    {
+      value: "w-2/5",
+      label: "『 w-2/5 』 width: 40%;"
+    },
+    {
+      value: "w-3/5",
+      label: "『 w-3/5 』 width: 60%;"
+    },
+    {
+      value: "w-4/5",
+      label: "『 w-4/5 』 width: 80%;"
+    },
+    {
+      value: "w-1/6",
+      label: "『 w-1/6 』 width: 16.666667%;"
+    },
+    {
+      value: "w-2/6",
+      label: "『 w-2/6 』 width: 33.333333%;"
+    },
+    {
+      value: "w-3/6",
+      label: "『 w-3/6 』 width: 50%;"
+    },
+    {
+      value: "w-4/6",
+      label: "『 w-4/6 』 width: 66.666667%;"
+    },
+    {
+      value: "w-5/6",
+      label: "『 w-5/6 』 width: 83.333333%;"
+    },
+    {
+      value: "w-1/12",
+      label: "『 w-1/12 』 width: 8.333333%;"
+    },
+    {
+      value: "w-2/12",
+      label: "『 w-2/12 』 width: 16.666667%;"
+    },
+    {
+      value: "w-3/12",
+      label: "『 w-3/12 』 width: 25%;"
+    },
+    {
+      value: "w-4/12",
+      label: "『 w-4/12 』 width: 33.333333%;"
+    },
+    {
+      value: "w-5/12",
+      label: "『 w-5/12 』 width: 41.666667%;"
+    },
+    {
+      value: "w-6/12",
+      label: "『 w-6/12 』 width: 50%;"
+    },
+    {
+      value: "w-7/12",
+      label: "『 w-7/12 』 width: 58.333333%;"
+    },
+    {
+      value: "w-8/12",
+      label: "『 w-8/12 』 width: 66.666667%;"
+    },
+    {
+      value: "w-9/12",
+      label: "『 w-9/12 』 width: 75%;"
+    },
+    {
+      value: "w-10/12",
+      label: "『 w-10/12 』 width: 83.333333%;"
+    },
+    {
+      value: "w-11/12",
+      label: "『 w-11/12 』 width: 91.666667%;"
+    },
+    {
+      value: "w-full",
+      label: "『 w-full 』 width: 100%;"
+    },
+    {
+      value: "w-screen",
+      label: "『 w-screen 』 width: 100vw;"
+    },
+    {
+      value: "w-min",
+      label: "『 w-min 』 width: min-content;"
+    },
+    {
+      value: "w-max",
+      label: "『 w-max 』 width: max-content;"
+    },
+    {
+      value: "w-fit",
+      label: "『 w-fit 』 width: fit-content;"
+    }
+  ]
+};
+
+export const MinWidth: AttributeProps = {
+  type: "min-width",
+  header: "Min-Width 属性",
+  title: "Min-Width ClassName：",
+  inputPlaceholder: "min-width: xxxx",
+  selectPlaceholder: "min-width: xxxx",
+  hasCustom: true,
+  options: [
+    {
+      value: "min-w-0",
+      label: "『 min-w-0 』 min-width: 0px;"
+    },
+    {
+      value: "min-w-full",
+      label: "『 min-w-full 』 min-width: 100%;"
+    },
+    {
+      value: "min-w-min",
+      label: "『 min-w-min 』 min-width: min-content;"
+    },
+    {
+      value: "min-w-max",
+      label: "『 min-w-max 』 min-width: max-content;"
+    },
+    {
+      value: "min-w-fit",
+      label: "『 min-w-fit 』 min-width: fit-content;"
+    }
+  ]
+};
+
+export const MaxWidth: AttributeProps = {
+  type: "max-width",
+  header: "Max-Width 属性",
+  title: "Max-Width ClassName：",
+  inputPlaceholder: "max-width: xxxx",
+  selectPlaceholder: "max-width: xxxx",
+  hasCustom: true,
+  options: [
+    {
+      value: "max-w-0",
+      label: "『 max-w-0 』 max-width: 0rem; /* 0px */"
+    },
+    {
+      value: "max-w-none",
+      label: "『 max-w-none 』 max-width: none;"
+    },
+    {
+      value: "max-w-xs",
+      label: "『 max-w-xs 』 max-width: 20rem; /* 320px */"
+    },
+    {
+      value: "max-w-sm",
+      label: "『 max-w-sm 』 max-width: 24rem; /* 384px */"
+    },
+    {
+      value: "max-w-md",
+      label: "『 max-w-md 』 max-width: 28rem; /* 448px */"
+    },
+    {
+      value: "max-w-lg",
+      label: "『 max-w-lg 』 max-width: 32rem; /* 512px */"
+    },
+    {
+      value: "max-w-xl",
+      label: "『 max-w-xl 』 max-width: 36rem; /* 576px */"
+    },
+    {
+      value: "max-w-2xl",
+      label: "『 max-w-2xl 』 max-width: 42rem; /* 672px */"
+    },
+    {
+      value: "max-w-3xl",
+      label: "『 max-w-3xl 』 max-width: 48rem; /* 768px */"
+    },
+    {
+      value: "max-w-4xl",
+      label: "『 max-w-4xl 』 max-width: 56rem; /* 896px */"
+    },
+    {
+      value: "max-w-5xl",
+      label: "『 max-w-5xl 』 max-width: 64rem; /* 1024px */"
+    },
+    {
+      value: "max-w-6xl",
+      label: "『 max-w-6xl 』 max-width: 72rem; /* 1152px */"
+    },
+    {
+      value: "max-w-7xl",
+      label: "『 max-w-7xl 』 max-width: 80rem; /* 1280px */"
+    },
+    {
+      value: "max-w-full",
+      label: "『 max-w-full 』 max-width: 100%;"
+    },
+    {
+      value: "max-w-min",
+      label: "『 max-w-min 』 max-width: min-content;"
+    },
+    {
+      value: "max-w-max",
+      label: "『 max-w-max 』 max-width: max-content;"
+    },
+    {
+      value: "max-w-fit",
+      label: "『 max-w-fit 』 max-width: fit-content;"
+    },
+    {
+      value: "max-w-prose",
+      label: "『 max-w-prose 』 max-width: 65ch;"
+    },
+    {
+      value: "max-w-screen-sm",
+      label: "『 max-w-screen-sm 』 max-width: 640px;"
+    },
+    {
+      value: "max-w-screen-md",
+      label: "『 max-w-screen-md 』 max-width: 768px;"
+    },
+    {
+      value: "max-w-screen-lg",
+      label: "『 max-w-screen-lg 』 max-width: 1024px;"
+    },
+    {
+      value: "max-w-screen-xl",
+      label: "『 max-w-screen-xl 』 max-width: 1280px;"
+    },
+    {
+      value: "max-w-screen-2xl",
+      label: "『 max-w-screen-2xl 』 max-width: 1536px;"
+    }
+  ]
+};
+
+export const Height: AttributeProps = {
+  type: "max-width",
+  header: "Max-Width 属性",
+  title: "Max-Width ClassName：",
+  inputPlaceholder: "max-width: xxxx",
+  selectPlaceholder: "max-width: xxxx",
+  hasCustom: true,
+  options: [
+    {
+      value: "h-0",
+      label: "『 h-0 』 height: 0px;"
+    },
+    {
+      value: "h-px",
+      label: "『 h-px 』 height: 1px;"
+    },
+    {
+      value: "h-0.5",
+      label: "『 h-0.5 』 height: 0.125rem; /* 2px */"
+    },
+    {
+      value: "h-1",
+      label: "『 h-1 』 height: 0.25rem; /* 4px */"
+    },
+    {
+      value: "h-1.5",
+      label: "『 h-1.5 』 height: 0.375rem; /* 6px */"
+    },
+    {
+      value: "h-2",
+      label: "『 h-2 』 height: 0.5rem; /* 8px */"
+    },
+    {
+      value: "h-2.5",
+      label: "『 h-2.5 』 height: 0.625rem; /* 10px */"
+    },
+    {
+      value: "h-3",
+      label: "『 h-3 』 height: 0.75rem; /* 12px */"
+    },
+    {
+      value: "h-3.5",
+      label: "『 h-3.5 』 height: 0.875rem; /* 14px */"
+    },
+    {
+      value: "h-4",
+      label: "『 h-4 』 height: 1rem; /* 16px */"
+    },
+    {
+      value: "h-5",
+      label: "『 h-5 』 height: 1.25rem; /* 20px */"
+    },
+    {
+      value: "h-6",
+      label: "『 h-6 』 height: 1.5rem; /* 24px */"
+    },
+    {
+      value: "h-7",
+      label: "『 h-7 』 height: 1.75rem; /* 28px */"
+    },
+    {
+      value: "h-8",
+      label: "『 h-8 』 height: 2rem; /* 32px */"
+    },
+    {
+      value: "h-9",
+      label: "『 h-9 』 height: 2.25rem; /* 36px */"
+    },
+    {
+      value: "h-10",
+      label: "『 h-10 』 height: 2.5rem; /* 40px */"
+    },
+    {
+      value: "h-11",
+      label: "『 h-11 』 height: 2.75rem; /* 44px */"
+    },
+    {
+      value: "h-12",
+      label: "『 h-12 』 height: 3rem; /* 48px */"
+    },
+    {
+      value: "h-14",
+      label: "『 h-14 』 height: 3.5rem; /* 56px */"
+    },
+    {
+      value: "h-16",
+      label: "『 h-16 』 height: 4rem; /* 64px */"
+    },
+    {
+      value: "h-20",
+      label: "『 h-20 』 height: 5rem; /* 80px */"
+    },
+    {
+      value: "h-24",
+      label: "『 h-24 』 height: 6rem; /* 96px */"
+    },
+    {
+      value: "h-28",
+      label: "『 h-28 』 height: 7rem; /* 112px */"
+    },
+    {
+      value: "h-32",
+      label: "『 h-32 』 height: 8rem; /* 128px */"
+    },
+    {
+      value: "h-36",
+      label: "『 h-36 』 height: 9rem; /* 144px */"
+    },
+    {
+      value: "h-40",
+      label: "『 h-40 』 height: 10rem; /* 160px */"
+    },
+    {
+      value: "h-44",
+      label: "『 h-44 』 height: 11rem; /* 176px */"
+    },
+    {
+      value: "h-48",
+      label: "『 h-48 』 height: 12rem; /* 192px */"
+    },
+    {
+      value: "h-52",
+      label: "『 h-52 』 height: 13rem; /* 208px */"
+    },
+    {
+      value: "h-56",
+      label: "『 h-56 』 height: 14rem; /* 224px */"
+    },
+    {
+      value: "h-60",
+      label: "『 h-60 』 height: 15rem; /* 240px */"
+    },
+    {
+      value: "h-64",
+      label: "『 h-64 』 height: 16rem; /* 256px */"
+    },
+    {
+      value: "h-72",
+      label: "『 h-72 』 height: 18rem; /* 288px */"
+    },
+    {
+      value: "h-80",
+      label: "『 h-80 』 height: 20rem; /* 320px */"
+    },
+    {
+      value: "h-96",
+      label: "『 h-96 』 height: 24rem; /* 384px */"
+    },
+    {
+      value: "h-auto",
+      label: "『 h-auto 』 height: auto;"
+    },
+    {
+      value: "h-1/2",
+      label: "『 h-1/2 』 height: 50%;"
+    },
+    {
+      value: "h-1/3",
+      label: "『 h-1/3 』 height: 33.333333%;"
+    },
+    {
+      value: "h-2/3",
+      label: "『 h-2/3 』 height: 66.666667%;"
+    },
+    {
+      value: "h-1/4",
+      label: "『 h-1/4 』 height: 25%;"
+    },
+    {
+      value: "h-2/4",
+      label: "『 h-2/4 』 height: 50%;"
+    },
+    {
+      value: "h-3/4",
+      label: "『 h-3/4 』 height: 75%;"
+    },
+    {
+      value: "h-1/5",
+      label: "『 h-1/5 』 height: 20%;"
+    },
+    {
+      value: "h-2/5",
+      label: "『 h-2/5 』 height: 40%;"
+    },
+    {
+      value: "h-3/5",
+      label: "『 h-3/5 』 height: 60%;"
+    },
+    {
+      value: "h-4/5",
+      label: "『 h-4/5 』 height: 80%;"
+    },
+    {
+      value: "h-1/6",
+      label: "『 h-1/6 』 height: 16.666667%;"
+    },
+    {
+      value: "h-2/6",
+      label: "『 h-2/6 』 height: 33.333333%;"
+    },
+    {
+      value: "h-3/6",
+      label: "『 h-3/6 』 height: 50%;"
+    },
+    {
+      value: "h-4/6",
+      label: "『 h-4/6 』 height: 66.666667%;"
+    },
+    {
+      value: "h-5/6",
+      label: "『 h-5/6 』 height: 83.333333%;"
+    },
+    {
+      value: "h-full",
+      label: "『 h-full 』 height: 100%;"
+    },
+    {
+      value: "h-screen",
+      label: "『 h-screen 』 height: 100vh;"
+    },
+    {
+      value: "h-min",
+      label: "『 h-min 』 height: min-content;"
+    },
+    {
+      value: "h-max",
+      label: "『 h-max 』 height: max-content;"
+    },
+    {
+      value: "h-fit",
+      label: "『 h-fit 』 height: fit-content;"
+    }
+  ]
+};
+
+export const MinHeight: AttributeProps = {
+  type: "min-height",
+  header: "Min-Height 属性",
+  title: "Min-Height ClassName：",
+  inputPlaceholder: "min-height: xxxx",
+  selectPlaceholder: "min-height: xxxx",
+  hasCustom: true,
+  options: [
+    {
+      value: "min-h-0",
+      label: "『 min-h-0 』 min-height: 0px;"
+    },
+    {
+      value: "min-h-full",
+      label: "『 min-h-full 』 min-height: 100%;"
+    },
+    {
+      value: "min-h-screen",
+      label: "『 min-h-screen 』 min-height: 100vh;"
+    },
+    {
+      value: "min-h-min",
+      label: "『 min-h-min 』 min-height: min-content;"
+    },
+    {
+      value: "min-h-max",
+      label: "『 min-h-max 』 min-height: max-content;"
+    },
+    {
+      value: "min-h-fit",
+      label: "『 min-h-fit 』 min-height: fit-content;"
+    }
+  ]
+};
+
+export const MaxHeight: AttributeProps = {
+  type: "max-height",
+  header: "Max-Height 属性",
+  title: "Max-Height ClassName：",
+  inputPlaceholder: "max-height: xxxx",
+  selectPlaceholder: "max-height: xxxx",
+  hasCustom: true,
+  options: [
+    {
+      value: "max-h-0",
+      label: "『 max-h-0 』 max-height: 0px;"
+    },
+    {
+      value: "max-h-px",
+      label: "『 max-h-px 』 max-height: 1px;"
+    },
+    {
+      value: "max-h-0.5",
+      label: "『 max-h-0.5 』 max-height: 0.125rem; /* 2px */"
+    },
+    {
+      value: "max-h-1",
+      label: "『 max-h-1 』 max-height: 0.25rem; /* 4px */"
+    },
+    {
+      value: "max-h-1.5",
+      label: "『 max-h-1.5 』 max-height: 0.375rem; /* 6px */"
+    },
+    {
+      value: "max-h-2",
+      label: "『 max-h-2 』 max-height: 0.5rem; /* 8px */"
+    },
+    {
+      value: "max-h-2.5",
+      label: "『 max-h-2.5 』 max-height: 0.625rem; /* 10px */"
+    },
+    {
+      value: "max-h-3",
+      label: "『 max-h-3 』 max-height: 0.75rem; /* 12px */"
+    },
+    {
+      value: "max-h-3.5",
+      label: "『 max-h-3.5 』 max-height: 0.875rem; /* 14px */"
+    },
+    {
+      value: "max-h-4",
+      label: "『 max-h-4 』 max-height: 1rem; /* 16px */"
+    },
+    {
+      value: "max-h-5",
+      label: "『 max-h-5 』 max-height: 1.25rem; /* 20px */"
+    },
+    {
+      value: "max-h-6",
+      label: "『 max-h-6 』 max-height: 1.5rem; /* 24px */"
+    },
+    {
+      value: "max-h-7",
+      label: "『 max-h-7 』 max-height: 1.75rem; /* 28px */"
+    },
+    {
+      value: "max-h-8",
+      label: "『 max-h-8 』 max-height: 2rem; /* 32px */"
+    },
+    {
+      value: "max-h-9",
+      label: "『 max-h-9 』 max-height: 2.25rem; /* 36px */"
+    },
+    {
+      value: "max-h-10",
+      label: "『 max-h-10 』 max-height: 2.5rem; /* 40px */"
+    },
+    {
+      value: "max-h-11",
+      label: "『 max-h-11 』 max-height: 2.75rem; /* 44px */"
+    },
+    {
+      value: "max-h-12",
+      label: "『 max-h-12 』 max-height: 3rem; /* 48px */"
+    },
+    {
+      value: "max-h-14",
+      label: "『 max-h-14 』 max-height: 3.5rem; /* 56px */"
+    },
+    {
+      value: "max-h-16",
+      label: "『 max-h-16 』 max-height: 4rem; /* 64px */"
+    },
+    {
+      value: "max-h-20",
+      label: "『 max-h-20 』 max-height: 5rem; /* 80px */"
+    },
+    {
+      value: "max-h-24",
+      label: "『 max-h-24 』 max-height: 6rem; /* 96px */"
+    },
+    {
+      value: "max-h-28",
+      label: "『 max-h-28 』 max-height: 7rem; /* 112px */"
+    },
+    {
+      value: "max-h-32",
+      label: "『 max-h-32 』 max-height: 8rem; /* 128px */"
+    },
+    {
+      value: "max-h-36",
+      label: "『 max-h-36 』 max-height: 9rem; /* 144px */"
+    },
+    {
+      value: "max-h-40",
+      label: "『 max-h-40 』 max-height: 10rem; /* 160px */"
+    },
+    {
+      value: "max-h-44",
+      label: "『 max-h-44 』 max-height: 11rem; /* 176px */"
+    },
+    {
+      value: "max-h-48",
+      label: "『 max-h-48 』 max-height: 12rem; /* 192px */"
+    },
+    {
+      value: "max-h-52",
+      label: "『 max-h-52 』 max-height: 13rem; /* 208px */"
+    },
+    {
+      value: "max-h-56",
+      label: "『 max-h-56 』 max-height: 14rem; /* 224px */"
+    },
+    {
+      value: "max-h-60",
+      label: "『 max-h-60 』 max-height: 15rem; /* 240px */"
+    },
+    {
+      value: "max-h-64",
+      label: "『 max-h-64 』 max-height: 16rem; /* 256px */"
+    },
+    {
+      value: "max-h-72",
+      label: "『 max-h-72 』 max-height: 18rem; /* 288px */"
+    },
+    {
+      value: "max-h-80",
+      label: "『 max-h-80 』 max-height: 20rem; /* 320px */"
+    },
+    {
+      value: "max-h-96",
+      label: "『 max-h-96 』 max-height: 24rem; /* 384px */"
+    },
+    {
+      value: "max-h-full",
+      label: "『 max-h-full 』 max-height: 100%;"
+    },
+    {
+      value: "max-h-screen",
+      label: "『 max-h-screen 』 max-height: 100vh;"
+    },
+    {
+      value: "max-h-min",
+      label: "『 max-h-min 』 max-height: min-content;"
+    },
+    {
+      value: "max-h-max",
+      label: "『 max-h-max 』 max-height: max-content;"
+    },
+    {
+      value: "max-h-fit",
+      label: "『 max-h-fit 』 max-height: fit-content;"
+    }
+  ]
+};
